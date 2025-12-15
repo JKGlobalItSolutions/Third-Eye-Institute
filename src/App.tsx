3671487411a1bd819174e576fb999a56a356df21
nive-dev <nivethithakesavan7823@gmail.com>
2025-12-16 04:48:39 +0530

    Configure GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="//Third-Eye-Institute/">
+      <BrowserRouter basename="/Third-Eye-Institute/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
